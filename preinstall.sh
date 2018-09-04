82fcf4eec5820cfb128db2956e571e5a2069e826
CH <ginsongsong@honghutech.com>
2018-09-04 19:48:57 +0800

    Update preinstall.sh
    Add doecker cache versions

--- preinstall.sh
+++ preinstall.sh
@@ -14,3 +14,19 @@ sudo apt-get install -y nvidia-docker2
 sudo pkill -SIGHUP dockerd
 
 
+#     18.06.1~ce~3-0~ubuntu 
+#     18.06.0~ce~3-0~ubuntu    
+#     18.03.1~ce-0~ubuntu 
+#     18.03.0~ce-0~ubuntu 
+#     17.12.1~ce-0~ubuntu 
+#     17.12.0~ce-0~ubuntu 
+#     17.09.1~ce-0~ubuntu 
+#     17.09.0~ce-0~ubuntu 
+#     17.06.2~ce-0~ubuntu
+#     17.06.1~ce-0~ubuntu  
+#     17.06.0~ce-0~ubuntu 
+#     17.03.3~ce-0~ubuntu-xenial 
+#     17.03.2~ce-0~ubuntu-xenial 
+#     17.03.1~ce-0~ubuntu-xenial 
+#     17.03.0~ce-0~ubuntu-xenial 
+ 
